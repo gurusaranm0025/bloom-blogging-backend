1ee642a01304eefcbfb02771c3749b775e03db07
Guru Saran M <gurusaranm0025@gmail.com>
2024-02-08 00:02:10 +0530

    migrated all server actions from frintend to backend

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ let app =
     : admin.apps[0];
 
 const server = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 server.use(express.json());
 server.use(bodyParser.urlencoded({ extended: true }));
